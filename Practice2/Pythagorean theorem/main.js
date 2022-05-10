f090ff67ff0594ca40e36cb73234f86009c7983f
dandrej13 <dimitrievsk.andrej@hotmail.com>
2022-05-10 17:41:11 +0200

    essentials

---
+++ Practice2/Pythagorean theorem/main.js
@@ -0,0 +1,39 @@
+let a = 36;
+let b = 15;
+//c = ?
+console.log(Math.sqrt(a ** 2 + b ** 2));
+
+let c = Math.sqrt(a ** 2 + b ** 2);
+
+
+document.getElementById(`a`).innerHTML = `a = ${a}`;
+document.getElementById(`b`).innerHTML = `b = ${b}` ;
+document.getElementById(`c`).innerHTML = `Result: c = ${c}`;
+
+
+let y = 3;
+let x = 4;
+let z = 5;
+
+
+console.log(y + x);
+console.log(z / y);
+console.log(z % x);
+
+
+let nr1 = 3;
+let nr2 = 4;
+
+console.log(`${nr1} + ${nr2} = `, nr1 + nr2)
+
+
+let mile = 209.2142;
+const conversionMToKm = 1.60934;
+let mToKm = mile / conversionMToKm;
+console.log(mToKm);
+
+let km = 130;
+const conversionRatio = 1.60934;
+console.log(`The distance of ${km} is equal to ${km * conversionRatio} miles`)
+
+
---
+++ Practice2/main.js
@@ -0,0 +1,39 @@
+let nr1 = 10;
+let nr2 = 4;
+
+let result = nr1 * nr2;
+
+console.log(result);
+console.log(nr1 + " * " + nr2 + " = " + result );
+
+result =  nr1 / nr2
+
+console.log(result);
+console.log(nr1 + " / " + nr2 + " = " + result );
+
+let result1 = nr1 % nr2;
+console.log(`${nr1} % ${nr2} = ${result1}`);
+console.log(`${nr1} * ${nr2} = ${nr1 * nr2}`);
+
+// Declartion of a number
+let incNumber = 1;
+// incremenation of a number
+incNumber++;
+// logging the number in the console
+console.log(incNumber);
+
+// Declartion of a number
+incNumber = 2;
+// decremenation of a number
+incNumber++;
+// logging the number in the console
+console.log(incNumber);
+
+
+//
+
+let num1 = 4;
+let num2 = 5;
+let num3 = 2;
+
+console.log(num1++ + ++num2 * num3++)
